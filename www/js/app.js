1b7e7864d62c2f8d3720035fe6e7099e600da323
Deepanshu Agarwal <deepanshu@cyware.com>
2016-10-06 14:20:26 +0530

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,25 @@
+app.constant("Config", {
+  "WebUrl": "http://news.ascian.in/api/",
+  "AppName" : "Ascian News",
+  "AndroidAppUrl" : "https://play.google.com/store/apps/details?id=com.myspecialgames.advanced2048game",
+  "ErrorMessage" : "End of results"
+})
+// config contact
+app.constant("ConfigContact", {
+  "EmailId": "ascian.india@gmail.com",
+  "ContactSubject": "Contact"
+})
+// config admon
+app.constant("ConfigAdmob", {
+  "interstitial": "ca-app-pub-3940256099942544/1033173712",
+  "banner": "ca-app-pub-3940256099942544/6300978111"
+})
+// color variations
+app.constant("Color", {
+  "AppColor": "positive", //light, stable, positive, calm, balanced, energized, assertive, royal, dark
+})
+// push notification
+app.constant("PushNoti", {
+  "senderID": "855184226097",
+})
+
