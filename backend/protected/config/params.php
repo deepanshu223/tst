5b41093d908abb822a490f58d51de16fff323ead
Deepanshu Agarwal <deepanshu@cyware.com>
2016-10-06 14:29:15 +0530

    adding backend code

---
+++ backend/protected/config/params.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'site_url'=> 'http://news.ascian.in', // website url without leading /
+	'static_path'=> 'http://news.ascian.in/static/', // should be site_url/static/ // Don't change change it without changing uploading script
+	'site_name'=> 'Ascian News',
+	'site_title'=> 'Ascian News is a popular website for reading tiny startup news',
+	'site_description'=> 'Ascian News is a popular website for reading tiny startup news',
+	'post_count' => '10',
+	// misc values
+	'facebook'=> 'https://www.facebook.com/ascianindia',
+	'API_KEY'=> 'AIzaSyAofpLEtzNq7fCCbaDYz1wzrzOLt3HnDRA',
+);
